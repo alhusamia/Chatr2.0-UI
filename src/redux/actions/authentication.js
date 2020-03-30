06d82452e01f5743ae56cf75b02f6c2353b683b6
PC <fdhfdh>
2020-03-30 10:15:38 +0300

    adding the authenticatin

--- src/redux/actions/authentication.js
+++ src/redux/actions/authentication.js
@@ -1,17 +1,62 @@
-import jwt_decode from "jwt-decode";
-
 import instance from "./instance";
 
-import {} from "./actionTypes";
+import { SET_CURRENT_USER } from "./actionTypes";
 
 import { setErrors } from "./errors";
 
-export const checkForExpiredToken = () => {};
+export const checkForExpiredToken = () => {
+  const token = localStorage.getItem("token");
 
-export const login = userData => {};
+  if (token) {
+    const currentTimeInSeconds = Date.now() / 1000;
 
-export const signup = userData => {};
+    const user = decode(token);
 
-export const logout = () => {};
+    if (user.exp >= currentTimeInSeconds) {
+      return setCurrentUser(token);
+    }
+  }
 
-const setCurrentUser = token => {};
+  return setCurrentUser();
+};
+
+export const login = userData => async dispatch => {
+  try {
+    const res = await instance.post("/login/", userData);
+    const { token } = res.data;
+
+    setAuthToken(token);
+    dispatch(setCurrentUser(token));
+  } catch (error) {
+    dispatch(setErrors(error));
+  }
+};
+
+export const signup = userData => async dispatch => {
+  try {
+    const res = await instance.post("/signup/", userData);
+    const { token } = res.data;
+    setAuthToken(token);
+    dispatch(setCurrentUser(token));
+  } catch (error) {
+    dispatch(setErrors(error));
+  }
+};
+
+export const logout = () => setCurrentUser();
+
+const setCurrentUser = token => {
+  setAuthToken(token);
+  const user = token ? decode(token) : null;
+  return { type: SET_CURRENT_USER, payload: user };
+};
+
+const setAuthToken = token => {
+  if (token) {
+    localStorage.setItem("token", token);
+    instance.defaults.headers.Authorization = `jwt ${token}`;
+  } else {
+    delete instance.defaults.headers.Authorization;
+    localStorage.removeItem("token");
+  }
+};
