575ded3f01a29d24b689f2b83a861a8b353bd26e
Annaguzel <you@example.com>
2020-03-30 13:24:10 +0300

    add messages to channel

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,8 +3,11 @@ import { combineReducers } from "redux";
 // Reducers
 import userReducer from "./user";
 import errorReducer from "./errors";
+import messagesReducer from "./channel"
 
 export default combineReducers({
   user: userReducer,
-  errors: errorReducer
+  errors: errorReducer,
+  messages:messagesReducer
 });
+
