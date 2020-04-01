d17839b1e6cbe383818c3ef406b2f29d045870fb
Olga <hijazin.olga@gmail.com>
2020-03-31 17:02:31 -0800

    Margin fixes for windows

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,20 +1,13 @@
-
 import React from "react";
 
 const MessageList = props => {
   return (
-   
-      <div className="px-3 py-3 mb-3 rounded-sm border border-white mr-5">
-        <small className="text-success">
-          <h6>{props.msg.username}</h6>
-        </small>
-        <h5
-          className="text-left"
-        >
-          {props.msg.message}
-        </h5>
-      </div>
-  
+    <div className="px-3 py-3 mb-3 rounded-sm border border-white mr-5 ml-5">
+      <small className="text-success">
+        <h6>{props.msg.username}</h6>
+      </small>
+      <h5 className="text-left">{props.msg.message}</h5>
+    </div>
   );
 };
 export default MessageList;
