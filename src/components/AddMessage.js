eff82119fa65b894fb3d5b35a713f7bcfcff1f52
PC <fdhfdh>
2020-04-02 15:19:00 +0300

    fixing

--- src/components/AddMessage.js
+++ src/components/AddMessage.js
@@ -102,7 +102,7 @@ class AddMessage extends Component {
                   )}
                 </span>
               </div>
-              <divgit >
+              <div >
                 <span>
                   {this.state.showGif ? (
                     <span ref={el => (this.gifPicker = el)}>
